b5aea330d3cf025bb518ff8607a823f5c2260ea2
mastermaxx03 <srivastavaanimesh22@gmail.com>
2025-04-09 21:33:02 +0530

    Connected to Database

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
+
+// Register routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
@@ -13,9 +16,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-// FIX: use `port`, not `PORT`
-app.listen({ port: Number(PORT), hostname: "localhost" }, () => {
-  console.log(`Subscription tracker is running on http://localhost:${PORT}`);
+// 👇 First connect to the database, then start the server
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker Api is running on http://localhost:${PORT}`
+  );
+  await connectToDatabase();
 });
 
 export default app;
